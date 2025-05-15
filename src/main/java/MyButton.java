dfe02a93a6a7af6059f897840b7e8f721bd77bb7
Kevin <pintokevin2002@hotmail.com>
2025-05-15 18:10:45 +0200

    Add constructor and clicked method documentation for MyButton

--- src/main/java/MyButton.java
+++ src/main/java/MyButton.java
@@ -2,12 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyButton extends JButton {
+
+    /**
+     * Constructor for MyButton.
+     * @param text The text to display on the button.
+     */
     MyButton(String text) {
         this.setText(text);
         this.setFocusable(false);
         this.setFont(new Font("Arial", Font.BOLD, 20));
     }
 
+    /**
+     * This method is called when the button is clicked.
+     * @param newText The new text to set on the button.
+     */
     public void clicked(String newText) {
         //this.setEnabled(false);
         this.setText(newText);
